7f4e1134c0deb04b26c010509d59f8c464b1927d
Joyel <Varghesejoyel71@gmail.com>
2025-05-08 01:10:01 +0530

    Address validation updated

--- src/components/AadhaarUpload/ParsedDataView.tsx
+++ src/components/AadhaarUpload/ParsedDataView.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 import { AadhaarData } from "../../types/Aadhaar";
+import { fieldMappings } from "../../utils/constants";
 import styles from "../../components/AadhaarUpload/AadhaarUpload.module.css";
 
 interface Props {
   data: AadhaarData;
-  extractTextAfterSIO: (text: string) => string; 
 }
 
-const ParsedDataView: React.FC<Props> = ({ data, extractTextAfterSIO }) => {
-  const address = data.address || "EE en Em N aR Rafts gga Sifter As 4 URGuEIGEnCationAuhoRty of India ZZ KAGHAAR Address EE oe or TE aE SIO K X Varghese, Komaroth House, Pattath ai mer ei any, ER Road, Chalikkavattom, Vennala, PO re SVennala";
-  const textAfterSIO = extractTextAfterSIO(address);
-
-  return (
-    <div className={styles.dataBox}>
-      <div className={styles.dataGrid}>
-        <div className={styles.dataField}>
-          <label className={styles.dataLabel}>Address:</label>
-          <span className={styles.dataValue}>{textAfterSIO}</span>
+const ParsedDataView: React.FC<Props> = ({ data }) => (
+  <div className={styles.dataBox}>
+    <h3 className={styles.dataTitle}>Parsed Data</h3>
+    <div className={styles.dataGrid}>
+      {Object.entries(fieldMappings).map(([label, key], index) => (
+        <div key={index} className={styles.dataField}>
+          <label className={styles.dataLabel}>{label}:</label>
+          <span className={styles.dataValue}>{data?.[key] || "N/A"}</span>
         </div>
-      </div>
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default ParsedDataView;
